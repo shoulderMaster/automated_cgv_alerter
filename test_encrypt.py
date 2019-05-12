cf6a90ebdf70afaebebebb6ce067b8f2d65be060
shoulderMaster <ckdqja0225@gmail.com>
2019-05-12 15:02:32 +0900

    find out the encryption algorithm by which CGV encrypt parameters

--- test_encrypt.py
+++ test_encrypt.py
@@ -3,36 +3,39 @@ import base64
 
 correct_input = "20190514"
 correct_output = "uU8HUn3rxCYRKgs3pGahCA=="
+cgv_aes_key = bytes([ 86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54, 79, 66, 70, 55, 49, 53, 56, 56, 57, 57, 67, 84, 52, 70, 51, 67 ])
+cgv_aes_IV = bytes([ 86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54 ])
 
 class cgv_crypto :
-  def __init__(self) :
+  def __init__(self, key, IV, mode) :
     self.BLOCK_SIZE = 16
-    self.padding_list = ["0", "\0", " "]
+    self.encryption_mode = mode
+    self.mode = mode
+    self.key = key
+    self.initial_vector = IV
+    self.encryptor = self.make_encryptor()
 
-  def get_padded_list(self, key_component) :
-    key_list = [(key_component+pad*16)[:self.BLOCK_SIZE] for pad in self.padding_list]
-    key_list.append((key_component*16)[:16])
-    return key_list
+  #PKCS7 padding algorithm
+  def pad(self, data) :
+    padding = bytes([(16-len(data))%16])
+    padded_data = (data+padding*16)[:16]
+    return padded_data
 
-  def make_encryptor(self, key) :
-    cipher = AES.new(key)
-    #return lambda plain_text : base64.b64encode(cipher.encrypt(plain_text.encode("utf8"))) 
-    return lambda plain_text : base64.b64encode(cipher.encrypt(plain_text)) 
+  def make_encryptor(self) :
+    if self.mode == AES.MODE_ECB :
+      cipher = AES.new(self.key, self.mode)
+    else :
+      cipher = AES.new(self.key, self.mode, self.initial_vector)
+    return lambda plain_text : base64.b64encode(cipher.encrypt(self.pad(plain_text.encode("utf8")))) 
     
   def encrypt(self, plain_text) :
-    plain_text = plain_text.encode("utf8")
-    #padded_plain_list = self.get_padded_list(plain_text)
-    #key_list = padded_plain_list
-    key_list = [plain_text]
-    padded_plain_list = [plain_text]
-    encryptor_list = [self.make_encryptor(key) for key in key_list]
-    encrypted_list = [enc(padded_plain_list[idx]) for idx, enc in enumerate(encryptor_list)]
-    print(encrypted_list)
-    return encrypted_list
+    encrypted_text = self.encryptor(plain_text)
+    return encrypted_text
 
 def main() :
-  crypto = cgv_crypto()
-  crypto.encrypt(correct_input)
+  crypto = cgv_crypto(key=cgv_aes_key, IV=cgv_aes_IV, mode=AES.MODE_CBC)
+  enc_txt = crypto.encrypt(correct_input)
+  print("encrypted text : %s" % enc_txt)
   print("correct output : %s" % correct_output)
   print("input : %s" % correct_input)
 
