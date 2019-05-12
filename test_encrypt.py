fca3d5baf9db24207781577f57fd4b7012a1147d
shoulderMaster <ckdqja0225@gmail.com>
2019-05-12 16:45:28 +0900

    add docstrings which describe how the parameter encryptor is implemented with detail

--- test_encrypt.py
+++ test_encrypt.py
@@ -1,10 +1,28 @@
 from Crypto.Cipher import AES
 import base64
 
+'''
+  CGV reservation web service use AES which is a kind of symetric key encryption algorithm in order to securely pass parameters containing private or even confidential information when client makes a reservation for a movie on the website. 
+  of particular interest is how the encryption algorithm is implemented. .net javascript framework has been so used to implement the encryption that it was not difficult to find out detailed specification of the AES algorithm with the microsoft .net framework official document and with reverse enginearing client-side script.
+  AES algorithm cannot be competible of communication among different services without any specification of implementation detail such as block size, encryption key, block cipher modes of operation, initial vector, padding method etc.
+  the CGV AES detailed specification is the table below.
+
+  block size        : 16byte (128 bits)
+  key size          : 16byte (same as block size)
+  encryption key    : VBA1NUS6T8UID2I6OBF7158899CT4F3C
+  initial_vector    : VBA1NUS6T8UID2I6
+  block cipher mode : CBC
+  encoding          : utf8
+  wrapper           : base64
+  padding           : PKCS7 (padding with sequence of bytes, each of which is equal to the size of padding string added)
+
+
+'''
+
 correct_input = "20190514"
 correct_output = "uU8HUn3rxCYRKgs3pGahCA=="
-cgv_aes_key = bytes([ 86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54, 79, 66, 70, 55, 49, 53, 56, 56, 57, 57, 67, 84, 52, 70, 51, 67 ])
-cgv_aes_IV = bytes([ 86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54 ])
+cgv_aes_key = bytes([86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54, 79, 66, 70, 55, 49, 53, 56, 56, 57, 57, 67, 84, 52, 70, 51, 67])
+cgv_aes_IV = bytes([86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54])
 
 class cgv_crypto :
   def __init__(self, key, IV, mode) :
@@ -17,7 +35,7 @@ class cgv_crypto :
 
   #PKCS7 padding algorithm
   def pad(self, data) :
-    padding = bytes([(16-len(data))%16])
+    padding = bytes([(self.BLOCK_SIZE-len(data))%self.BLOCK_SIZE])
     padded_data = (data+padding*16)[:16]
     return padded_data
 
