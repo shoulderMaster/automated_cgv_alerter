9004a5105362eb81d84b0fd02ee6d8bea34a1fb9
shoulderMaster <ckdqja0225@gmail.com>
2019-05-12 18:55:04 -0700

    add a rootine which initialize IV for each encryption into encrypt() method

--- cgv_crypto.py
+++ cgv_crypto.py
@@ -2,10 +2,10 @@ from Crypto.Cipher import AES
 import base64
 
 '''
-  CGV web service use AES which is a kind of symetric key encryption algorithm in order to securely pass parameters containing private or even confidential information when client makes a reservation for a movie on the website. 
-  of particular interest is how the encryption algorithm is implemented. .net javascript framework has been so used to implement the encryption that it was not difficult to find out detailed specification of the AES algorithm with the microsoft .net framework official document and with reverse enginearing client-side script.
+  CGV web service use AES which is a kind of symetric key encryption algorithm in order to securely pass parameters containing private or even confidential information when client makes a reservation for a movie on the website.
+  of particular interest is how the encryption algorithm is implemented. .net javascript framework had been used to implement the encryption so that it was not difficult to find out detailed specification of the AES algorithm with the microsoft .net framework official document and with reverse enginearing client-side script.
   AES algorithm cannot be competible of communication among different services without any specification of implementation detail such as block size, encryption key, block cipher modes of operation, initial vector and padding method etc.
-  the CGV AES detailed specification is the table below.
+  the CGV AES detailed specification is described as the table below.
 
   block size        : 16byte (128 bits)
   key size          : 32byte (256 bits)
@@ -24,7 +24,7 @@ correct_output = "uU8HUn3rxCYRKgs3pGahCA=="
 cgv_aes_key = bytes([86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54, 79, 66, 70, 55, 49, 53, 56, 56, 57, 57, 67, 84, 52, 70, 51, 67])
 cgv_aes_IV = bytes([86, 66, 65, 49, 78, 85, 83, 54, 84, 56, 85, 73, 68, 50, 73, 54])
 
-class cgv_crypto :
+class CGV_AES :
   def __init__(self, block_size=16, key=cgv_aes_key, IV=cgv_aes_IV, mode=AES.MODE_CBC) :
     self.BLOCK_SIZE = block_size
     self.encryption_mode = mode
@@ -45,8 +45,10 @@ class cgv_crypto :
       cipher = AES.new(self.key, self.mode)
     else :
       cipher = AES.new(self.key, self.mode, self.initial_vector)
-    return lambda plain_text : base64.b64encode(cipher.encrypt(self.pad(plain_text.encode("utf8")))) 
-    
+    return lambda plain_text : base64.b64encode(cipher.encrypt(self.pad(plain_text.encode("utf8"))))
+
   def encrypt(self, plain_text) :
     encrypted_text = self.encryptor(plain_text)
+    #initialize initial vector
+    self.encryptor = self.make_encryptor()
     return encrypted_text.decode("utf8")
